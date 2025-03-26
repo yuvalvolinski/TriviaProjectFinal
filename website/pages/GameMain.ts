a4db84643092c7618e628f621903209f3ba5d2ac
yuvalvolinski <yuvalvolinski5@gmail.com>
2025-03-26 09:45:59 +0200

    rules

--- website/pages/GameMain.ts
+++ website/pages/GameMain.ts
@@ -1,8 +1,18 @@
 let bLogOut = document.getElementById("bLogOut")!;
+let bRules = document.getElementById("bRules")!;
+let bBack = document.getElementById("bBack")!;
 
 
 bLogOut.onclick = function () {
      window.location.href = "main.html"
   };
 
+
+bRules.onclick = function () {
+   window.location.href = "rules.html"
+}
+
+bBack.onclick = function () {
+   window.location.href = "GameMain.html"
+}
   
